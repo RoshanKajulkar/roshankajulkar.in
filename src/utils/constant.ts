53f815aa30bbf3583c171f687107c84086be3aa8
Roshan Kajulkar <roshankajulkar@gmail.com>
2025-07-14 17:56:03 +0530

    Update constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 const LINKS = {
-    LINKED_IN: "https://www.linkedin.com/in/roshankajulkar",
+    LINKED_IN: "https://www.linkedin.com/in/roshan-kajulkar",
     GITHUB: "https://github.com/RoshanKajulkar",
     LEETCODE: "https://leetcode.com/u/RoshanKajulkar",
     X: "https://x.com/roshankajulkar"
